b545071ddb7c77646b287da3368ce5fb3a876ea9
Thmc6511 <thmc6511@colorado.edu>
2024-07-15 16:49:32 -0600

    Update Movement.h

--- Movement.h
+++ Movement.h
@@ -4,9 +4,35 @@
 #include <string>
 using namespace std;
 
-class Map{
-    private:
-    
-    public:
+const string MAGENTA = "\033[35m";
+const string GREEN = "\033[32m";
+const string BLUE = "\033[34m";
+const string ORANGE = "\033[33m";
+const string GREY = "\033[90m";
+const string RESET = "\033[0m";
+
+class Map {
+public:
+    // Constructor
+    Map();
+
+    // Public methods
+    void Setup();
+    void DrawMap();
+    void UserInput();
+    void MovementLogic();
+
+private:
+    // Constants
+    static const int width = 150;
+    static const int length = 20;
+
+    // Private methods
+    void CharacterMenu(const std::string& interaction, const std::string& option1, const std::string& invalidMsg);
+
+    // Private variables
+    bool gameOver;
+    int x, y, xTree, yTree, xIron, yIron, xcharNPC, ycharNPC, xPortal, yPortal;
 };
-#endif
+
+#endif // MAP_H
