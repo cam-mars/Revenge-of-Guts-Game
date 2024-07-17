6342226ba693b0351e8c349d1e940207cac69f62
cam-mars <cameronjmars23@gmail.com>
2024-07-17 09:14:46 -0600

    Create proj2Library.h

---
+++ proj2Library.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include "Hero.h"
+#include "Sword.h"
+#include "Resources.h"
+#include "csci1300Library.h"
+using namespace std;
+
+void drawMap(int xPlayer, int yPlayer);
+void statsDisplay(Hero myHero, Resources myResources);
+int moveCharacter();
